eb15786c8d675acf005ceec809bdb89e93ee9740
Devon Mack <devon.mack@shopify.com>
2020-02-08 14:32:24 -0500

    list

---
+++ utilities.js
@@ -0,0 +1,16 @@
+// import jsdom from 'jsdom';
+// const jsdom = require("jsdom");
+// const fetch = require("node-fetch");
+
+// const { JSDOM } = jsdom;
+
+// async function getSongs() {
+//   const songs = await fetch('https://www.azchords.com/a/adamsandler-tabs-113/odetomycar-tabs-205998.html',);
+
+//   const dom = new JSDOM(await songs.text());
+//   console.log(dom.window.document.querySelector('pre').textContent);
+
+//   return [{key:'a'},{key:'b'},{key:'c'},{key:'d'},{key:'e'}];
+// }
+
+// getSongs();
